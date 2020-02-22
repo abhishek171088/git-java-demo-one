f686beb00cd34829615bb358d5831b0bd1830cc4
Abhishek <abhishek.kr1710@gmail.com>
2020-02-22 11:13:42 +0530

    Changes done for spring boot app

--- JavaGitDemo/src/main/java/com/Application.java
+++ JavaGitDemo/src/main/java/com/Application.java
@@ -1,14 +1,21 @@
 package com;
 
-public class Application {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("abc!!");
-		System.out.println("Hello!!");
-		System.out.println("World!!!");
-		System.out.println("11");
-		
+		SpringApplication.run(Application.class, args);
 
 	}
 
