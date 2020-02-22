8777b444a715692eafc8ff40cecfecf19e943f6b
Abhishek <abhishek.kr1710@gmail.com>
2020-02-22 10:04:12 +0530

    changes done

--- JavaGitDemo/src/main/java/com/Application.java
+++ JavaGitDemo/src/main/java/com/Application.java
@@ -4,10 +4,8 @@ public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("abc!!");
-		System.out.println("Hello!!");
+		System.out.println("Hello");
 		System.out.println("World!!!");
-		
 		
 
 	}
